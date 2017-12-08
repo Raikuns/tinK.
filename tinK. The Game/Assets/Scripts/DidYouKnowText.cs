77ebd2c92911099affc92c641fd1007cf12adee0
Raikuns <joopwestra@gmail.com>
2017-12-08 13:30:54 +0100

    Build 1.1.2:21231

---
+++ tinK. The Game/Assets/Scripts/DidYouKnowText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DidYouKnowText : MonoBehaviour {
+
+    public TMP_Text text;
+
+    public string[] didyouknows;
+
+    private void Start()
+    {
+        text.text = didyouknows[(Mathf.RoundToInt(Random.Range(0, didyouknows.Length - 1)))];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            text.text = didyouknows[(Mathf.RoundToInt(Random.Range(0, didyouknows.Length - 1)))];
+        }
+    }
+}
---
+++ tinK. The Game/Assets/Scripts/LoadManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadManager : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TMP_Text progressText;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync (int sceneIndex)
+    {
+        AsyncOperation Operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!Operation.isDone)
+        {
+            float progress = Mathf.Clamp01(Operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+
+        }
+    }
+}
